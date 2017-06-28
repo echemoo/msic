285a77bd88de61b7e690a06b26af0156cf524458
echemoo <echemoo@gmail.com>
2017-06-29 00:38:25 +0800

    Typing : add Hilbert R-Tree c++ code. first file of block file #2

--- Typing/hrtree/blk_file.h
+++ Typing/hrtree/blk_file.h
@@ -19,6 +19,21 @@ class BlockFile
     ~BlockFile();
 
     void put_bytes(char* bytes, int num){fwrite(bytes, num, 1, fp);}
+    void get_bytes(char* bytes, int num){fread(bytes, num, 1, fp);}
+    void fwrite_number(int num);
+    int fread_number();
+    void seek_block(int bnum){fseek(fp, (bnum-act_block) * blocklength, SEEK_CUR);}
+    void read_header(char* header);
+    void set_header(char* header);
+    bool read_block(Block b, int i);
+    bool write_block(Block b, int i);
+    int append_block(Block b);
+    bool delete_last_blocks(int num);
+    bool file_new(){return new_flag;}
+    int get_blocklength(){return blocklength;}
+    int get_num_of_blocks(){return number;}
 };
+
+
 
 #endif // __BLK_FILE
