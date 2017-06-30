79cd18ea6218780ec7d03012c4f749588699d383
echemoo <echemoo@gmail.com>
2017-06-30 22:18:30 +0800

    add Hilbert R-Tree c++ code, block file implement sources of blk_file.cc 2@#

--- Typing/hrtree/blk_file.cc
+++ Typing/hrtree/blk_file.cc
@@ -77,4 +77,53 @@ void BlockFile::set_header(char* header){
     act_block = 1;
 }
 
+bool BlockFile::read_block(Block b, int pos){ // read block data located at pos position.
+  pos++;    // external block to internal block
+  if (pos <= number && pos > 0)
+    seek_block(pos);
+  else {
+    printf("Requested block %d is illegal.", pos - 1);
+    error("\n", true);
+  }
+
+  get_bytes(b, blocklength);
+  if (pos + 1 > number) {   // if pos is the last block on disk
+    fseek(fp, 0, SEEK_SET);
+    act_block = 0;  // if the file pointer reaches the end, rewind to the beginning of data file
+  }
+  else
+    act_block = pos + 1;    // take act_block to next block location of file after read this block data
+
+  return TRUE;
+}
+
+bool BlockFile::write_block(Block block, int pos){
+  pos++;    // external # to interal #
+
+  if (pos <= number && pos > 0)
+    seek_block(pos);
+  else {
+    printf("Requested block %d is illegal.", pos - 1);  error("\n", true);
+  }
+  put_bytes(block, blocklength);
+  if (pos + 1 > number) {
+    fseek(fp, 0, SEEK_SET);
+    act_block=0;
+  }
+  else
+    act_block = pos + 1;
+
+  return TRUE;
+}
+
+int BlockFile::append_block(Block block) {
+  fseek(fp, 0, SEEK_END);
+  put_bytes(block, blocklength);
+  number++;
+  fseek(fp, sizeof(int), SEEK_SET);
+  fwrite_number(number);
+  fseek(fp, -blocklength, SEEK_END);
+
+  return (act_block = number) - 1;
+}
 
