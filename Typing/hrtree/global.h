302619510c865e08166f5e44540dcf43a401b94d
echemoo <echemoo@gmail.com>
2017-07-06 23:48:09 +0800

    add Hirbert R-Tree C++ code. Provide some util functions of global in global.h 2#

--- Typing/hrtree/global.h
+++ Typing/hrtree/global.h
@@ -19,4 +19,34 @@ float MbrMINDIST(float* _m1, float* _m2, int _dim);
 float MbrMAXDIST(float* _m1, float* _m2, int _dim);
 float mindist(float* p, float* bounces, int dimension, float* bound);   // by Greg
 float mindist_dynamic(float* p, float* bounces, int dimension, float* bound, bool* func);   // by Greg
+float minmaxdist(float* Point, float* bounces, int dimension, float* bound);    // by Greg
+float minmaxdist_dynamic(float* p, float* bounces, int dimension, float* bound, bool* func);    // by Greg
+bool inside(float& p, float& lb, float& ub);
+void enlarge(int dimension, float** mbr, float* r1, float* r2);
+bool inside(float* v, float* mbr, int dimension);
+bool mbr_inside(float* mbr1, float* mbr2, int dimension);   // by Greg
+bool mbr_inside_dynamic(float* mbr1, float* mbr2, int dimension, bool* func);   // by Greg
+bool point_inside_mbr(float* p, float* mbr2, int dimension);    // by Greg
+bool boundedFound(float* bounces, float* bound, int dimension);     // by Greg
+bool boundedFound_dynamic(float* bounces, float* bound, int dimension, bool* func);     // by Greg
+bool cornerFound(float* bounces, float* bound, int dimension);  // by Greg
+void printTime(long elapsed);   // by Greg
+int pruneBrunchList(float* nearest_distanz, const void* activebrunchList, int n);
+int boundedpruneBrunchList(float* nearest_distanz, const void* activebrunchList, int n);    // by Greg
+float* overlapRectPt(int dimension, float* r1, float* r2);  // by Greg
+bool section(int dimension, float* mbr1, float* mbr2);
+bool section_dynamic(int dimension, float* mbr1, float* mbr2, bool* func);  // by Greg
+bool section_c(int dimension, float* mbr1, float* center, float radius);
+int sort_lower_mbr(const void* d1, const void* d2);
+int sort_upper_mbr(const void* d1, const void* d2);
+int sort_center_mbr(const void* d1, const void* d2);
+int sortmindist(const void* element1, const void* element2);
+int sort_tuple_score(const void* d1, const void* d2);   // by Greg
+
+#ifdef UNIX
+void strupr(char* _msg);
+#endif
+
+float* mindis_point(float* p, float* bounces, int _dim);
+float mindist_value(float* p, float* mbb, int dim);
 
